5e8a389bd74b25b5071871a27632d1233a51f820
Anderson B. Silva <anderson.barbosa@dotz.com>
2022-01-13 15:35:12 -0300

    carregar bundles js conforme layer (team, early-adopters, production ...)

---
+++ src/declarations.d.ts
@@ -0,0 +1,7 @@
+declare module "@hyp/common" {
+  const emitEvent: any;
+  const counterSubject: any;
+  const listenEvent: any;
+
+  export { emitEvent, listenEvent, counterSubject };
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,29 @@
 import Layout from "../Layout";
+import { emitEvent, counterSubject } from '@hyp/common'
 
-const Dashboard = () => (
-  <Layout>
+const Dashboard = () => {
+  const handleClickByCustomEvent = event => {
+    emitEvent('@hyp/beauty/counter/increment', { incrementalValue: 1 })
+  }
+
+  const handleClickByRxjs = event => {
+    counterSubject.next(1)
+  }
+
+  return (<Layout>
     <section className="hero is-dark welcome is-small">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title">My Beauty Dashboard</h1>
+          <h1 className="title">My Beauty Dashboard (Production Version)</h1>
           <h2 className="subtitle">texto de exemplo...</h2>
         </div>
       </div>
     </section>
+    <div className="buttons mt-5">
+      <button className="button is-info" onClick={handleClickByCustomEvent}>Incrementar utilizando Custom Event</button>
+      <button className="button is-success" onClick={handleClickByRxjs}>Incrementar utilizando RxJS</button>
+    </div>
   </Layout>
-);
+)};
 
 export default Dashboard;
